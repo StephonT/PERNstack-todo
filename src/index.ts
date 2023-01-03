43965230908d44f1e035fe5afc9490e7b6bbd6b1
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2023-01-03 13:00:51 -0500

    created server

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import { json, urlencoded } from "body-parser";
+
+const app = express();
+
+const port = 9001;
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server is ready at http://localhost:${port}👾✨`);
+});
+
+app.get("/", (_, res) => {
+  res.send("Hello World!");
+});
+
+export default app;
