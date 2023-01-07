318ee55c1219fcfce1ee7e30823107f6484dc074
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2023-01-07 12:26:21 -0500

    created theme file

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,10 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NewTodo from "./NewTodo";
 import Todos from "./Todos";
 import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from "../theme";
 
 const App: React.FC = () => {
   return (
-  <ChakraProvider> 
+  <ChakraProvider theme={theme}> 
     <Router>
       <Routes>
         <Route path="/new" element={<NewTodo />} />
